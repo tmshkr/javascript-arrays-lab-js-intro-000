51ea79d948ff036010ed2276a8011aabffdb1dfc
Tim Shaker <tmshkr@users.noreply.github.com>
2018-06-30 00:42:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,39 +3,33 @@
 var kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(name){
-    
+    kittens.push(name)
 }
 
 function destructivelyPrependKitten(name){
-    
+
 }
 
 function destructivelyRemoveLastKitten(){
-    
+
 }
 
 function destructivelyRemoveFirstKitten(){
-    
+
 }
 
 function appendKitten(name){
-    
+
 }
 
 function prependKitten(name){
-    
+
 }
 
 function removeLastKitten(){
-    
+
 }
 
 function removeFirstKitten(){
-    
+
 }
-
-
-
-
-
-
