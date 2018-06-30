a2a958938a1b7ddb7b1f8d527e130907156d90a4
Tim Shaker <tmshkr@users.noreply.github.com>
2018-06-30 00:41:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
 //const app = "I don't do much."
 
 var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name){
+    
+}
+
+function destructivelyPrependKitten(name){
+    
+}
+
+function destructivelyRemoveLastKitten(){
+    
+}
+
+function destructivelyRemoveFirstKitten(){
+    
+}
+
+function appendKitten(name){
+    
+}
+
+function prependKitten(name){
+    
+}
+
+function removeLastKitten(){
+    
+}
+
+function removeFirstKitten(){
+    
+}
+
+
+
+
+
+
