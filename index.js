0788dd40611f1c581020fbacadb550ce506e4655
Tim Shaker <tmshkr@users.noreply.github.com>
2018-06-30 00:44:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ function destructivelyPrependKitten(name){
 }
 
 function destructivelyRemoveLastKitten(){
-
+    kittens.pop()
 }
 
 function destructivelyRemoveFirstKitten(){
-
+    kittens.shift()
 }
 
 function appendKitten(name){
